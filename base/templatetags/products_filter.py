8203dc31877ad4e2696b5bae9bad4c2071363be9
farrar142 <gksdjf1690@gmail.com>
2022-01-09 23:41:39 +0900

    session cookies

--- base/templatetags/products_filter.py
+++ base/templatetags/products_filter.py
@@ -5,3 +5,14 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+def multiple(value, arg):
+    try:
+        value = int(value)
+    except:
+        pass
+    try:
+        arg = int(arg)
+    except:
+        pass
+    result = str(value*arg)
+    return result
