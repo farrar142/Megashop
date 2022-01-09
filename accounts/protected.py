cffa3125c5fc1744f4bceac9fb7ebe06d7a5f7e2
farrar142 <gksdjf1690@gmail.com>
2022-01-09 21:41:52 +0900

    asd

--- accounts/protected.py
+++ accounts/protected.py
@@ -1,2 +1,2 @@
 KAKAO_APP__REST_API_KEY="153710cf03c998ae8f42004cf835b833"
-KAKAO_APP__LOGIN__REDIRECT_URI="http://megashop.honeycombpizza/accounts/signin/kakao/callback"
+KAKAO_APP__LOGIN__REDIRECT_URI="https://site1.honeycombpizza/accounts/signin/kakao/callback"
