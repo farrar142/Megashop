2647c9b58b2c7ce9ab414fcc1e117e26e0cf0056
farrar142 <gksdjf1690@gmail.com>
2022-01-10 11:03:17 +0900

    asd

--- deploy.py
+++ deploy.py
@@ -189,6 +189,7 @@ def deploy():
     try:
         test_con = Container(con_info)
     except:
+        os.system(f"docker rmi -f {cur_image_name}")
         raise Exception("ImageBuildFailed Please Check tests.py files or Requirements Setting")
     if connection_checker(test_con) == False:
         os.system(f"docker rm -f {test_con.container_name}")
