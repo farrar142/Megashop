f4c0cbb10e5f4fa5ec39f108f0542959d5883289
farrar142 <gksdjf1690@gmail.com>
2022-01-10 18:16:41 +0900

    .env 추가

--- deploy.py
+++ deploy.py
@@ -4,6 +4,8 @@ import subprocess
 import time
 import socket
 from datetime import datetime
+from pathlib import Path
+
 def deploy():
     cur_time = get_now()
     path = get_setting_path()
@@ -267,8 +269,16 @@ def make_deploy_logs(deploylogfile):
     f = open(f"{deploylogfile}","w")
     f.write("")
     f.close()
-
+def env_getter():
+    path = Path(__file__).resolve().parent
+    print(path)
+    f = open(f"{str(path.parent)}/.env","r",encoding="UTF-8")
+    target = open(f"{str(path)}/.env","w",encoding="UTF-8")
+    target.write(f.read())
+    target.close()
+    f.close()
 def main():
+    env_getter()
     deploy()
 
 if __name__ == "__main__":
--- pwd.py
+++ pwd.py
@@ -1,10 +1,11 @@
 import os
 from pathlib import Path
-def main():
+def env_getter():
     path = Path(__file__).resolve().parent
-    f = open(f"{path.parent}/.env","r","UTF-8")
-    target = open(f"{path}/.env","w","UTF-8")
-    target.write(f)
+    print(path)
+    f = open(f"{str(path.parent)}/.env","r",encoding="UTF-8")
+    target = open(f"{str(path)}/.env","w",encoding="UTF-8")
+    target.write(f.read())
     target.close()
     f.close()
     
