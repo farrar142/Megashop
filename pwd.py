867971e568f491f47cbc1f769145e69600134a66
farrar142 <gksdjf1690@gmail.com>
2022-01-10 18:09:24 +0900

    pwd check

--- pwd.py
+++ pwd.py
@@ -1,9 +1,13 @@
 import os
 from pathlib import Path
 def main():
-    path = Path(__file__).resolve()
-    print(path)
-    print(path.parent)
+    path = Path(__file__).resolve().parent
+    f = open(f"{path.parent}/.env","r","UTF-8")
+    target = open(f"{path}/.env","w","UTF-8")
+    target.write(f)
+    target.close()
+    f.close()
+    
 
 if __name__=="__main__":
     main()
